9282032aa7e1005ad8ffac18c32b4212a890261e
Godfred Agyapong <125074936+gagyapong@users.noreply.github.com>
2025-03-06 22:21:29 -0500

    final papers

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -5,24 +5,24 @@ import yaml
 def test_get_conference_dates():
     conference = yaml.safe_load(
         """
-start_date: 2020-01-01
-end_date: 2020-01-01
+start_date: 2025-03-04
+end_date: 2025-03-05
     """
     )
-    assert get_conference_dates(conference) == "January 1"
+    assert get_conference_dates(conference) == "March 4"
 
     conference = yaml.safe_load(
         """
-start_date: 2020-01-01
-end_date: 2020-01-02
+start_date: 2025-03-04
+end_date: 2025-03-05
     """
     )
-    assert get_conference_dates(conference) == "January 1-2"
+    assert get_conference_dates(conference) == "March 4-5"
 
     conference = yaml.safe_load(
         """
-start_date: 2020-01-01
-end_date: 2020-02-02
+start_date: 2025-03-04
+end_date: 2025-03-05
     """
     )
-    assert get_conference_dates(conference) == "January 1 - February 2"
+    assert get_conference_dates(conference) == "March 3 - March 4"
